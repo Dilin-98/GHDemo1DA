2cc589508a7c95185da53f353932e38a1cc6797b
Aravind Gireesh Kumar - STUDENT <L00170985@atu.ie>
2022-09-19 11:48:06 +0100

    HelloWorld.java has been edited and added to repo

--- Code_Repo/HelloWorld.java
+++ Code_Repo/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloLYIT {
  public static void main(String[] args) {
  System.out.println("Hello, World"); // Prints "Hello,World" as output
+ System.out.println("Hello, LYIT"); //This line has been added by the partner
  }
 }
